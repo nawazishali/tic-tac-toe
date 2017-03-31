3e0224f9736192be8d8936e559b301415d90ddfa
Nawazish Ali <nawazishaliejaz@gmail.com>
2017-03-31 21:45:55 +0500

    Two Player logic working

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-/* jshint undef: true, unused: false, evil: true, esnext: true */
+/* jshint undef: true, unused: false, evil: true, esnext: true, expr: true */
 /* globals document, window, setInterval, clearInterval, */
 
 
 let aiPlayer = "X";
 let huPlayer = "O";
 let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let currentPlayer = huPlayer;
 
 //simple selector function.
 function $(elem) {
@@ -15,14 +16,28 @@ function $(elem) {
     }
 }
 
-function humanTurn() {
+function huTurn() {
     let cells = $("td");
     //Array.from converts HTMLcollection or Array like objects to actual Arrays.
     Array.from(cells).forEach((cell) => cell.onclick = () => {
         cell.innerHTML = huPlayer;
         board[cell.id] = huPlayer;
-        console.log(board);
+        cell.onclick = null;
+        aiTurn();
     });
 }
 
-humanTurn();
+function aiTurn() {
+    let cells = $("td");
+    //Array.from converts HTMLcollection or Array like objects to actual Arrays.
+    Array.from(cells).forEach((cell) => cell.onclick = () => {
+        cell.innerHTML = aiPlayer;
+        board[cell.id] = aiPlayer;
+        cell.onclick = null;
+        huTurn();
+
+    });
+}
+
+(currentPlayer === huPlayer) ? huTurn() : aiTurn();
+
